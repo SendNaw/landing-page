f867bf9c0ce2ea24f02b8ef95be97227229035b2
cas1908 <schima05@gmail.com>
2023-11-15 10:48:55 +0100

    fix: waitlist api, waitlist scroll button color

--- src/Components/Home/UserWaitlist.ts
+++ src/Components/Home/UserWaitlist.ts
@@ -1,31 +1,28 @@
+import axios from "axios";
 
+const useWaitlist = () => {
+  const post = (email: string) => {
+    const data = {
+      email: email,
+    };
 
-
-const useWaitlist = ()=>{
-    const post = (email:string)=> {
-        const data = {
-                    email: email
+    axios
+      .post("http://18.195.91.17:3000/subscribe", data,{
+        headers: {
+          'Content-Type': "application/json"
         }
-        
-        const dataPost = JSON.stringify(data)
-        console.log(dataPost)
-        fetch("https://sendnaw-api.onrender.com/subscribe", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: dataPost
-        })
-        .then(res => {
-            if (res.status === 200 ){ 
-            console.log("Success") 
-        }else {
-            console.log(res)
-        }})
-        .catch(err => console.log(err))
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res);
 
-    }
-    return { post }
-}
+        } else {
+          console.log(res);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return { post };
+};
 
 export default useWaitlist;
